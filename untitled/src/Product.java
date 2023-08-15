61997cf8d944e1d28eb83e53ace27a0e504a8567
ismayilSDET <ismayil.jumshudov@gmail.com>
2023-08-14 21:16:31 -0400

    Team Daniela 1st case

---
+++ untitled/src/Product.java
@@ -0,0 +1,30 @@
+
+public class Product {
+	
+	
+	/*- Description: Represents a product with attributes like name, price, and quantity.
+- Variables:
+  - `name` (String): The name of the product.
+  - `price` (double): The price of the product.
+  - `quantity` (int): The quantity of the product available.
+- Constructor that creates object with above attributes
+*/
+	
+	String name;
+	double price;
+	int quantity;
+	
+	public Product(String name, double price, int quantity) {
+		
+		this.name=name;
+		this.price=price;
+		this.quantity=quantity;
+		
+		
+	}
+	
+	
+	
+	
+
+}
